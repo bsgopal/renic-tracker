5281a74a2869b27795d94bf16ee326cc5e7ef38c
Gopalakrishnan a <126150922+bsgopal@users.noreply.github.com>
2025-08-24 18:53:35 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: true } // Railway uses SSL
+  ssl: { rejectUnauthorized: false } // Railway uses SSL
 });
 
 module.exports = pool;
